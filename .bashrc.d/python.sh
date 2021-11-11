020188e2c4197aefc58d7879a712b8352e90f7cd
Matthias Adler <macedigital@users.noreply.github.com>
2021-11-11 16:32:50 +0100

    python: no output buffers for stdout and stderr
    Setting environment variable `PYTHONUNBUFFERED` has the effect of forcing the stdout and stderr streams to be unbuffered.

--- .bashrc.d/python.sh
+++ .bashrc.d/python.sh
@@ -1,6 +1,7 @@
 # vim:ft=bash:ts=4:sw=4:et
 
 export PYTHONDONTWRITEBYTECODE=1
+export PYTHONUNBUFFERED=1
 
 if [[ -x $(type -p pip) ]]; then
     alias pip_upgrade='pip list -o --user --not-required --format=freeze | cut -d '=' -f1 | xargs pip install -U'
