453767692bbd802a8f37fcf54d64a8f2300e01c6
Matthias Adler <macedigital@users.noreply.github.com>
2022-02-20 16:04:23 +0100

    fish: setup interactive sessions in single place

---
+++ .config/fish/conf.d/interactive.fish
@@ -0,0 +1,40 @@
+# setup shell for interactive sessions
+if status is-interactive
+    set -gx XDG_CONFIG_HOME "$HOME/.config"
+    set -gx XDG_DATA_HOME "$HOME/.local/share"
+    set -gx XDG_CACHE_HOME "$HOME/.cache"
+    set -gx LESSHISTFILE /dev/null
+    set -gx GPG_TTY (tty)
+
+    if test -d ~/.local/bin
+        fish_add_path ~/.local/bin
+    end
+
+    if type -q lynx
+        set -gx BROWSER lynx
+    end
+
+    if type -q bat
+        # need to also set option when man already uses ANSI escape sequences
+        # see https://github.com/sharkdp/bat/issues/652#issuecomment-529032263
+        set -gx MANROFFOPT "-c"
+        set -gx MANPAGER "sh -c 'col -bx | bat -pl man'"
+    end
+
+    if type -q nnn
+        abbr ls nnn
+
+        set -gx NNN_OPTS "de"
+        set -gx NNN_PLUG "e:-!sudoedit \$nnn"
+        set -gx NNN_RCLONE "rclone mount --no-checksum"
+        set -gx NNN_SSHFS "sshfs -o reconnect,idmap=user,cache_timeout=3600"
+    end
+
+    if type -q direnv
+        direnv hook fish | source
+    end
+
+    if type -q starship
+        starship init fish | source
+    end
+end
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,18 +3,6 @@ set -g fish_private_mode private
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    if test -d ~/.local/bin
-        fish_add_path ~/.local/bin
-    end
-
-    # init tools for interactive sessions
-    if type -q direnv
-        direnv hook fish | source
-    end
-
-    if type -q starship
-        starship init fish | source
-    end
 end
 
 if status is-login
