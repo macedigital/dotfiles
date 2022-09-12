75a2d470d104867ace6f4a1a11a7b4dfbcfcfe81
Matthias Adler <macedigital@users.noreply.github.com>
2022-09-12 20:26:11 +0200

    nnn: provide default environment variables
    Set a number of default settings for `nnn` via environment variables.

--- .bashrc.d/nnn.sh
+++ .bashrc.d/nnn.sh
@@ -1,6 +1,11 @@
 # vim:ft=bash:ts=4:sw=4:et
 
 if [[ -x $(type -p nnn) ]]; then
-    alias ls="nnn -de"
+    alias ls="nnn"
     alias ncp="cat ${NNN_SEL:-${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.selection} | tr '\0' '\n'"
+
+    export NNN_PLUG="e:-!sudoedit \$nnn"
+    export NNN_OPTS="de"
+    export NNN_RCLONE="rclone mount --no-checksum"
+    export NNN_SSHFS="sshfs -o reconnect,idmap=user,cache_timeout=3600"
 fi
