94658eec86ad01f681ab7f06df0dab46d1e4204a
Matthias Adler <macedigital@users.noreply.github.com>
2023-10-14 20:41:55 +0200

    conf(nnn): add mpv based plugin
    Commit adds a new plugin called "addtoplaylist" which will queue the
    current nnn selection into mpv video player.
    
    Plugin script can be invoked from a shell or from nnn.

--- private_dot_bashrc.d/private_nnn.sh
+++ private_dot_bashrc.d/private_nnn.sh
@@ -3,9 +3,16 @@
 if [[ -x $(type -p nnn) ]]; then
     alias ncp="cat ${NNN_SEL:-${XDG_CONFIG_HOME:-$HOME/.config}/nnn/.selection} | tr '\0' '\n'"
 
-    export NNN_PLUG="e:-!sudoedit \$nnn;p:preview-tui"
+    # provide custom plugin that depends on `mpv` binary
+    addtoplaylist=""
+    if [[ -x $(type -p mpv) ]]; then
+        addtoplaylist="a:addtoplaylist;"
+    fi
+    export NNN_PLUG="${addtoplaylist}e:-!sudoedit \$nnn;p:preview-tui"
     export NNN_OPTS="acd"
     export NNN_RCLONE="rclone mount --no-checksum"
     export NNN_SSHFS="sshfs -o reconnect,idmap=user,cache_timeout=3600"
     export NNN_TRASH=2 # 1=trash-cli, 2=gio
+
+    unset addtoplaylist
 fi
