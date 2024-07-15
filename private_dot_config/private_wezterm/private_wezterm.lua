7d3bf44e8a508be4859754a20f3c971e9dc35965
Matthias Adler <macedigital@users.noreply.github.com>
2024-07-16 00:35:27 +0200

    conf(wezterm): remove default window padding

--- private_dot_config/private_wezterm/private_wezterm.lua
+++ private_dot_config/private_wezterm/private_wezterm.lua
@@ -44,5 +44,11 @@ config.hide_tab_bar_if_only_one_tab = true
 config.initial_cols = 112
 config.initial_rows = 40
 config.warn_about_missing_glyphs = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 return config
