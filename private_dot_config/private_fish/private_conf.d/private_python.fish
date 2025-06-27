52903bc3cf4581ae7996005e8959ce72434c4186
Matthias Adler <macedigital@users.noreply.github.com>
2025-06-27 12:43:54 +0200

    conf(fish): prefer uv over pyenv
    Skip setting up environment for using `pyenv` if `uv` is installed for managing Python projects.

--- private_dot_config/private_fish/private_conf.d/private_python.fish
+++ private_dot_config/private_fish/private_conf.d/private_python.fish
@@ -3,8 +3,11 @@ set -gx PYTHONDONTWRITEBYTECODE 1
 if status is-interactive
     set -gx PYTHONSTARTUP $XDG_CONFIG_HOME/python/pythonrc
 
-    # setup pyenv
-    if type -p $XDG_DATA_HOME/pyenv/bin/pyenv
+    # prefer uv over pyenv
+    if command -q uv
+        # no additional neeeded
+    else if -p $XDG_DATA_HOME/pyenv/bin/pyenv
+        # setup pyenv
         set -Ux PYENV_ROOT $XDG_DATA_HOME/pyenv
         set -U fish_user_paths $PYENV_ROOT/bin $fish_user_paths
 
