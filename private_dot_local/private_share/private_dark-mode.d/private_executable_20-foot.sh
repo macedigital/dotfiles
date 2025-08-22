1f87641b0230a79386ca999505cd766114e9be7f
Matthias Adler <macedigital@users.noreply.github.com>
2025-08-22 22:55:59 +0200

    feat(foot): adjust initial-color-theme
    Foot uses a fixed configuration setting for theme selection on startup.
    While switching colorscheme works for all running terminals, new
    terminals will start with pre-defined colors, not currently selected
    theme.
    
    Workaround this by also updating `initial-color-theme` in the
    configuration file itself.

--- private_dot_local/private_share/private_dark-mode.d/private_executable_20-foot.sh
+++ private_dot_local/private_share/private_dark-mode.d/private_executable_20-foot.sh
@@ -4,3 +4,6 @@ set -eu
 # https://codeberg.org/dnkl/foot/src/branch/master/CHANGELOG.md#1-23-1
 # Sending `SIGUSR2` sets to [color2] (aka. dark-mode)
 pgrep foot | xargs -r kill -s SIGUSR2
+
+# initial colorscheme is not dynamic, set up new terminals to use same color
+sed -i 's/initial-color-theme=1/initial-color-theme=2/' "$XDG_CONFIG_HOME"/foot/foot.ini
--- private_dot_local/private_share/private_light-mode.d/private_executable_20-foot.sh
+++ private_dot_local/private_share/private_light-mode.d/private_executable_20-foot.sh
@@ -4,3 +4,6 @@ set -eu
 # https://codeberg.org/dnkl/foot/src/branch/master/CHANGELOG.md#1-23-1
 # Sending `SIGUSR1` sets to [color] (aka. light-mode)
 pgrep foot | xargs -r kill -s SIGUSR1
+
+# initial colorscheme is not dynamic, set up new terminals to use same color
+sed -i 's/initial-color-theme=2/initial-color-theme=1/' "$XDG_CONFIG_HOME"/foot/foot.ini
