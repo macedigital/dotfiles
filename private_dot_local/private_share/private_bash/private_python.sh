8cb0f8e4afcaa4a8fb1fbe46e559598a82d25671
Matthias Adler <macedigital@users.noreply.github.com>
2025-09-01 11:32:26 +0200

    conf(bash): drop global 'pyenv' setup
    Remove global setup for `pyenv` as we're switching to `uv` based
    workflows. Pyenv can still be setup on a per project basis, e.g. using
    `direnv`, etc.

--- private_dot_local/private_share/private_bash/private_python.sh
+++ private_dot_local/private_share/private_bash/private_python.sh
@@ -3,29 +3,8 @@
 
 export PYTHONDONTWRITEBYTECODE=1
 export PYTHONUNBUFFERED=1
-export PYTHONSTARTUP=~/.config/python/pythonrc
+export PYTHONSTARTUP="$XDG_CONFIG_HOME"/python/pythonrc
 
 if [[ -x $(type -p pip) ]]; then
     alias pip_upgrade='pip list -o --user --not-required --format=freeze | cut -d '=' -f1 | xargs pip install -U'
-fi
-
-PYENV_ROOT="${PYENV_ROOT:-$HOME/.local/share/pyenv}"
-if [[ -x "${PYENV_ROOT}/bin/pyenv" ]]; then
-    export PYENV_ROOT
-    export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-    if ! [[ "$PATH" =~ "$PYENV_ROOT/bin:" ]]; then
-        export PATH="$PYENV_ROOT/bin:$PATH"
-    fi
-    # see https://github.com/pyenv/pyenv#advanced-configuration
-    eval "$(pyenv init --path)"
-
-    # init for pyenv running in interactive shells
-    if [[ $- == *i* ]]; then
-        eval "$(pyenv init -)"
-        if [[ $(pyenv commands | grep 'virtualenv-init') ]]; then
-            eval "$(pyenv virtualenv-init -)"
-        fi
-    fi
-else
-    unset PYENV_ROOT
 fi
