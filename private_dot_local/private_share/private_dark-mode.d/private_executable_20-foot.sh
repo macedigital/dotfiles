dee4780ff9adcb068ad5b8c480caf7657ec302ee
macedigital <macedigital@users.noreply.github.com>
2026-06-11 23:24:54 +0200

    conf(darkman): update light/dark initial theme for foot
    Update `darkman` hook scripts for dynamic patching of `foot.ini`

--- private_dot_local/private_share/private_dark-mode.d/private_executable_20-foot.sh
+++ private_dot_local/private_share/private_dark-mode.d/private_executable_20-foot.sh
@@ -2,8 +2,8 @@
 set -eu
 
 # initial colorscheme is not dynamic, set up new terminals to use same color
-sed -i 's/initial-color-theme=1/initial-color-theme=2/' "$HOME"/.config/foot/foot.ini
+sed -i 's/initial-color-theme=light/initial-color-theme=dark/' "$HOME"/.config/foot/foot.ini
 
 # https://codeberg.org/dnkl/foot/src/branch/master/CHANGELOG.md#1-23-1
-# Sending `SIGUSR2` sets to [color2] (aka. dark-mode)
-pgrep foot | xargs -r kill -s SIGUSR2
+# Sending `SIGUSR1` sets theme to dark-mode
+pgrep foot | xargs -r kill -s SIGUSR1
--- private_dot_local/private_share/private_light-mode.d/private_executable_20-foot.sh
+++ private_dot_local/private_share/private_light-mode.d/private_executable_20-foot.sh
@@ -2,8 +2,8 @@
 set -eu
 
 # initial colorscheme is not dynamic, set up new terminals to use same color
-sed -i 's/initial-color-theme=2/initial-color-theme=1/' "$HOME"/.config/foot/foot.ini
+sed -i 's/initial-color-theme=dark/initial-color-theme=light/' "$HOME"/.config/foot/foot.ini
 
 # https://codeberg.org/dnkl/foot/src/branch/master/CHANGELOG.md#1-23-1
-# Sending `SIGUSR1` sets to [color] (aka. light-mode)
-pgrep foot | xargs -r kill -s SIGUSR1
+# Sending `SIGUSR2` sets to light-mode
+pgrep foot | xargs -r kill -s SIGUSR2
